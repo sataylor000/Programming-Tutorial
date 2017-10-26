e4eff93ea9104d1981f440ecedfb4f376acc7e5e
Scott Taylor <s-taylor@att.net>
2017-10-26 08:32:01 -0400

    sync with master; complete Exercise1:  Hello, World!

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloWorld;
+
+/**
+ * @author Scott
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
